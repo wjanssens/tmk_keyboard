3faaca1d65722b69649b1e27d9f10cd6d16ec3c0
Warren Janssens <warren.janssens@gmail.com>
2015-02-12 21:55:05 -0700

    initial move into forked project

---
+++ keyboard/ergodox/keymap_wj.h
@@ -0,0 +1,111 @@
+/****************************************************************************************************
+*
+* Keymap: Default Layer in Dvorak
+*
+* ,--------------------------------------------------.           ,--------------------------------------------------.
+* |   =+   |  1!  |  2@  |  3#  |  4$  |  5%  |      |           |      |  6^  |  7&  |  8*  |  9(  |  0)  |   -_   |
+* |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
+* |   Tab  |  '"  |  ,<  |  .>  |   P  |   Y  |      |           |      |   J  |   F  |   U  |   P  |   $  |   /?   |
+* |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
+* |   Esc  |   A  |   O  |   E  |   U  |   I  |------|           |------|   Y  |   N  |   E  |   O  |   I  |   \|   |
+* |--------+------+------+------+------+------|  Fn  |           |  Fn  |------+------+------+------+------+--------|
+* |  Shift |  ;:  |   Q  |   J  |   K  |   X  |      |           |      |   B  |   M  |   W  |   V  |   Z  |  Shift |
+* `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
+*   |      | Shift| Ctrl | Alt  | Gui  |                                       | Gui  | Alt  | Ctrl | Shift|      |
+*   |      |  -_  |  =+  | Left | Right|                                       | Up   | Left |  [{  |  ]}  |      |
+*   `----------------------------------'                                       `----------------------------------'
+*                                        ,-------------.       ,-------------.
+*                                        | /?   | \|   |       | `~   | §±   |
+*                                 ,------|------|------|       |------+------+------.
+*                                 |      |      |      |       |      |      |      |
+*                                 | BkSp | Del  |------|       |------|Return| Space|
+*                                 |      |      | Esc  |       | Tab  |      |      |
+*                                 `--------------------'       `--------------------'
+*
+* Keymap: Fn/Keypad Layer
+*
+* ,--------------------------------------------------.           ,--------------------------------------------------.
+* |        |  F1  |  F2  |  F3  |  F4  |  F5  |      |           |      |  F6  |  F7  |  F8  |  F9  |  F10 |        |
+* |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
+* |        |  F11 |  F12 |  F13 |  F14 | Vol+ |      |           |      | Next |   7  |   8  |   9  |   -  |        |
+* |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
+* |  Caps  | PgUp | Home | Up   | End  | Mute |------|           |------| Play |   4  |   5  |   6  |   +  |        |
+* |--------+------+------+------+------+------| TRNS |           | TRNS |------+------+------+------+------+--------|
+* |  Shift | PgDn | Left | Down | Right| Vol- |      |           |      | Prev |   1  |   2  |   3  |   =  | Shift  |
+* `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
+*   |      | Shift| Ctrl | Home | End  |                                       | PgUp | PgDn |   .  |   /  |      |
+*   `----------------------------------'                                       `----------------------------------'
+*                                        ,-------------.       ,-------------.
+*                                        |      |Teensy|       |      |   =  |
+*                                 ,------|------|------|       |------+------+------.
+*                                 |      |      |      |       |      |      |      |
+*                                 |      | Ins  |------|       |------| KEnt |  K0  |
+*                                 |      |      | Caps |       | NL/Cl|      |      |
+*                                 `--------------------'       `--------------------'
+*
+*/
+
+static const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    /* Layer 0: Default */
+    KEYMAP(
+           EQL  ,1   ,2   ,3   ,4   ,5   ,NO  ,
+           TAB  ,QUOT,COMM,DOT ,P   ,Y   ,NO  ,
+           ESC  ,A   ,O   ,E   ,U   ,I   ,     
+           LSFT ,FN11,Q   ,J   ,K   ,X   ,FN5 ,
+           NO   ,FN1 ,FN2 ,FN3 ,FN4 ,          
+                                    SLSH,BSLS,
+                                         NO  ,
+                               BSPC,DEL ,ESC,
+           NO  ,6   ,7   ,8   ,9   ,0   ,MINS,
+           NO  ,F   ,G   ,C   ,R   ,L   ,SLSH,
+                D   ,H   ,T   ,N   ,S   ,BSLS,
+           FN6 ,B   ,M   ,W   ,V   ,FN12,RSFT,
+                     FN7 ,FN8 ,FN9 ,FN10,NO  ,
+           GRV ,NUBS,
+           NO  ,
+           TAB ,ENT ,SPC 
+    ),
+    /* Layer 1: Keypad */
+    KEYMAP(
+           NO  ,F1  ,F2  ,F3  ,F4  ,F5  ,NO  ,
+           NO  ,F11 ,F12 ,F13 ,F14 ,VOLU,NO  ,
+           NO  ,PGUP,HOME,UP  ,END ,MUTE,     
+           LSFT,PGDN,LEFT,DOWN,RGHT,VOLD,TRNS,
+           NO  ,LSFT,LCTL,HOME,END ,          
+                                    NO  ,FN0 ,
+                                         NO  ,
+                               NO  ,INS ,CAPS,
+           NO  ,F6  ,F7  ,F8  ,F9  ,F10 ,NO  ,
+           NO  ,MNXT,P7  ,P8  ,P9  ,PMNS,NO  ,
+                MPLY,P4  ,P5  ,P6  ,PPLS,NO  ,
+           TRNS,MPRV,P1  ,P2  ,P3  ,PAST,RSFT,
+                     PGUP,PGDN,PDOT,PSLS,NO  ,
+           NO  ,PEQL,
+           NO  ,
+           NLCK,PENT,P0
+    ),};
+enum function_id {
+    TEENSY_KEY,
+};
+static const uint16_t PROGMEM fn_actions[] = {
+	[0] = ACTION_FUNCTION(TEENSY_KEY),
+	[1] = ACTION_MODS_TAP_KEY(MOD_LSFT, KC_MINS),
+	[2] = ACTION_MODS_TAP_KEY(MOD_LCTL, KC_EQL),
+	[3] = ACTION_MODS_TAP_KEY(MOD_LALT, KC_LEFT),
+	[4] = ACTION_MODS_TAP_KEY(MOD_LGUI, KC_RGHT),
+	[5] = ACTION_LAYER_TAP_TOGGLE(1),
+	[6] = ACTION_LAYER_TAP_TOGGLE(1),
+	[7] = ACTION_MODS_TAP_KEY(MOD_RGUI, KC_UP),
+	[8] = ACTION_MODS_TAP_KEY(MOD_RALT, KC_DOWN),
+	[9] = ACTION_MODS_TAP_KEY(MOD_RCTL, KC_LBRC),
+	[10] = ACTION_MODS_TAP_KEY(MOD_RSFT, KC_RBRC),
+	[11] = ACTION_MODS_TAP_KEY(MOD_LSFT, KC_SCLN),
+	[12] = ACTION_MODS_TAP_KEY(MOD_RSFT, KC_Z)
+};
+void action_function(keyrecord_t *event, uint8_t id, uint8_t opt) {
+    if (id == TEENSY_KEY) {
+        clear_keyboard();
+        _delay_ms(250);
+        bootloader_jump();
+    }
+}
